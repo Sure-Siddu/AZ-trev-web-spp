77b2a12b4d4dae8fd41457e4858c47bebaefe4cf
Sure-Siddu <surendra19952025@hotmail.com>
2025-08-06 12:03:28 +0530

    added a staging branch

---
+++ AZ-trev-web-spp/Pages/DeploymentSolts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AZ_trev_web_spp.Pages
+{
+    public class DeploymentSoltsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
